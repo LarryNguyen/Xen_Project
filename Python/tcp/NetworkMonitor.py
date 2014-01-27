0571f2e2fa7c6e6e3349bb01be7bf4c8bb123ecc
tran hoang dai <tranhoangdainguyen@gmail.com>
2014-01-27 16:48:34 +0900

    count network packets

--- Python/tcp/NetworkMonitor.py
+++ Python/tcp/NetworkMonitor.py
@@ -4,11 +4,10 @@ Created on Jan 20, 2014
 @author: dai-network-lab
 '''
 
+
 import pprint
-import dpkt, pcap, inspect
-import sys
-import scapy
 from scapy.sendrecv import sniff
+import sys, time, sched
 
 
 class TcpMonitor(object):
@@ -16,42 +15,44 @@ class TcpMonitor(object):
     Tcp Monitoring class
     '''
 
-    def __init__(self, inf, _filter=""):
+    def __init__(self, iface, filter="",timeout=5):
         '''
         Constructor, need to have an network interface
         '''
-        self.inf = inf
-        self.filter = _filter
-        self.pc = pcap.pcap(name=self.inf)
-        if self.filter != "":
-            self.pc.setfilter(self.filter)
+        self.iface = iface
+        self.filter = filter
+        self.packets = None
+        self.s = None
+        self.timeout = timeout
+
+    def sniffPackets(self):
+        self.packets = sniff(iface=self.iface, filter=self.filter, timeout=self.timeout)
+
+
+    def intervalAction(self, packets):
+        print len(packets), "packets with filter (", self.filter, ") get captured"
+        '''
+            do something here
+        '''
+
+        # recapture the packets again
+        self.capture()
 
 
     def capture(self):
-       for ts, pkt in self.pc:
-            eth = dpkt.ethernet.Ethernet(pkt)
-            print `eth`
-            ip = eth.data
-            tcp = ip.data
-
-
-    def capture2(self):
-        a=sniff(filter="port 80", count=2)
-        a.nsummary()
-
+        self.s = sched.scheduler(time.time, self.sniffPackets())
+        self.s.enter(5, 1, self.intervalAction(self.packets), ())
+        self.s.run()
 
 if __name__ == "__main__":
     pp = pprint.PrettyPrinter(indent=2)
-    pp.pprint(len(sys.argv))
     if len(sys.argv) != 3:
         print "Usage:"
         print sys.argv[0], " <interface name> <filter>"
         sys.exit(1)
 
-
 #     pp.pprint(inspect.getmembers(pcap))
     monitor = TcpMonitor(sys.argv[1], sys.argv[2])
     monitor.capture()
-    monitor.capture2()
 
 
